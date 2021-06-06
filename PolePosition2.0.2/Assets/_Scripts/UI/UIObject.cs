8cd5f8bb00c853ae8ef59b6f5b03031115c02f44
Anthony <62327779+Anthonys-source@users.noreply.github.com>
2021-06-06 12:20:11 +0200

    Format UI

--- PolePosition2.0.2/Assets/_Scripts/UI/UIObject.cs
+++ PolePosition2.0.2/Assets/_Scripts/UI/UIObject.cs
@@ -12,16 +12,25 @@ namespace Game.UI
         [SerializeField] protected GameObject UIContainer;
         [HideInInspector] protected UIManager uiManager;
 
+        /// <summary>
+        /// Show the UI object
+        /// </summary>
         public void Show()
         {
             UIContainer.SetActive(true);
         }
 
+        /// <summary>
+        /// Hide the UI object
+        /// </summary>
         public void Hide()
         {
             UIContainer.SetActive(false);
         }
 
+        /// <summary>
+        /// Initialize the UIObject
+        /// </summary>
         public void SetUIManager(UIManager uiManager)
         {
             this.uiManager = uiManager;
